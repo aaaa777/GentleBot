d5fb236a073458bf08739b525fad83c55204ffb8
aaaa777 <aaaa777@users.noreply.github.com>
2023-11-19 18:31:10 +0900

    Refactor async_init_metadata method in Song class

--- Bot/music/song.py
+++ Bot/music/song.py
@@ -122,65 +122,6 @@ class Song:
 
         return metadata
 
-    async def async_init_metadata(self):
-        self.proc = 'awaiting'
-        await self.start_download_metadata(self.url)
-        # await self.start_download_metadata(self.url)
-        await self.sync_metadata()
-
-
-    async def start_download_metadata(self, url):
-        if self.proc != 'awaiting' and self.proc is not None:
-            return
-        
-        if url in self.metadata_cache:
-            return self.metadata_cache[url]
-        self.proc = await asyncio.create_subprocess_shell(' '.join(['yt-dlp', '--dump-json', '--format', 'bestaudio/best', url]), stdout=PIPE, stderr=STDOUT)
-
-    async def get_metadata(self):
-        if self.url in self.metadata_cache:
-            return self.metadata_cache[self.url]
-        
-        if self.proc is None:
-            return None
-        
-        if self.proc == 'awaiting':
-            while self.proc == 'awaiting':
-                await asyncio.sleep(1)
-
-        try:
-            # communicateメソッドでプロセスの終了を待ち、出力を取得します。
-            stdout, _ = await self.proc.communicate()
-            outs = stdout.decode()
-
-        except Exception as e:
-            print(e)
-            self.proc.kill()
-            self.proc = None
-            outs = "{'proc_status': 'error'}"
-        
-        metadata = json.loads(outs)
-        if 'proc_status' not in metadata:
-            metadata['proc_status'] = 'ok'
-        return metadata
-
-    async def sync_metadata(self):
-        
-        # metadata = ytdl.extract_info(url, download=False)
-        # metadata = self.get_download_metadata()
-        # loop = asyncio.get_event_loop()
-        # task = loop.create_task(self.get_metadata())
-        metadata = await self.get_metadata()
-        # print(metadata)
-
-
-
-        self.metadata_cache[self.url] = metadata
-        return metadata
-    
-    # async def await_data(self):
-    #     await self.metadata_cor
 
     def __str__(self):
-        # self.sync_metadata()
         return f'{self.title} - {self.duration // 60}:{(int)(self.duration % 60):02d}'
